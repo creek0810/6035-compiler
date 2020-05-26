b6a0cd26814d54fa2f9011867be21635feed698d
creek0810 <creek880810@gmail.com>
2020-05-26 14:01:58 +0800

    chore: add exe

---
+++ src/make.bat
@@ -0,0 +1,13 @@
+@echo off
+win_flex --header-file=tokenizer.h -o tokenizer.c tokenizer.l
+win_bison -d -o parser.c parser.y
+
+gcc -c cli.c -std=gnu99
+gcc -c parser.c -std=gnu99 
+gcc -c tokenizer.c -std=gnu99
+gcc -c symbolTable.c -std=gnu99
+gcc -c node.c -std=gnu99
+gcc -c interpreter.c -std=gnu99
+gcc -c object.c -std=gnu99
+gcc -c stack.c -std=gnu99
+gcc -o cli cli.o parser.o tokenizer.o symbolTable.o node.o interpreter.o object.o stack.o
